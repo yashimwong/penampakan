8374dfdbf23d6e56ed49990a1cfee083b628f679
yashimwong <yashimwong@gmail.com>
2026-08-10 09:00:00 +0800

    Publish the initial release surface

--- src/penampakan/backends/__init__.py
+++ src/penampakan/backends/__init__.py
@@ -2,5 +2,16 @@
 
 from penampakan.backends.callable import CallableVisionBackend
 from penampakan.backends.pillow import PillowBackend
+from penampakan.backends.tesseract import TesseractBackend
+from penampakan.backends.transformers import (
+    TransformersCaptionBackend,
+    TransformersDetectionBackend,
+)
 
-__all__ = ("CallableVisionBackend", "PillowBackend")
+__all__ = (
+    "CallableVisionBackend",
+    "PillowBackend",
+    "TesseractBackend",
+    "TransformersCaptionBackend",
+    "TransformersDetectionBackend",
+)
